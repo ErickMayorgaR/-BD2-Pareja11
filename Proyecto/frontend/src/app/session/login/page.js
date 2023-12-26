75aaa78bd913cab074683cd92b12844fba1961e8
ErickMayorgaR <66332670+ErickMayorgaR@users.noreply.github.com>
2023-12-25 22:04:04 -0600

    cambios en login

--- Proyecto/frontend/src/app/session/login/page.js
+++ Proyecto/frontend/src/app/session/login/page.js
@@ -51,47 +51,69 @@ const Login = () => {
   }, []);
 
   return (
-    <div className="login-container">
-      <div className="sub-login-container">
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+      
+      <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white-900">
+        Inicia Sesion en tu Cuenta
+      </h2>
+    </div>
 
-        <h1>Inicio de sesión</h1>
-
-        <form onSubmit={onSubmit}>
-          <div className="form-group">
-            <label htmlFor="email">Correo electrónico</label>
-
+    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form className="space-y-6" action="#" method="POST">
+        <div>
+          <label htmlFor="email" className="block text-sm font-medium leading-6 text-white-900">
+            Usuario
+          </label>
+          <div className="mt-2">
             <input
-              type="email"
               id="email"
               name="email"
-              className="form-control"
-              value={correo}
-              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              autoComplete="email"
               required
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             />
           </div>
-          <div className="form-group">
-            <label htmlFor="password">Contraseña</label>
+        </div>
 
-
+        <div>
+          <div className="flex items-center justify-between">
+            <label htmlFor="password" className="block text-sm font-medium leading-6 text-white-900">
+              Contraseña
+            </label>
+          </div>
+          <div className="mt-2">
             <input
-              type="password"
               id="password"
               name="password"
-              className="form-control"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              autoComplete="current-password"
               required
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             />
           </div>
-          <button type="submit">Iniciar sesión</button>
-          <p>
-            ¿No tienes una cuenta?
-            <a href="/register">Registrarse</a>
-          </p>
-        </form>
-      </div>
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Iniciar Sesion
+          </button>
+        </div>
+      </form>
+
+      <p className="mt-10 text-center text-sm text-gray-500">
+        No tienes una cuenta?{' '}
+        <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+          registrate
+        </a>
+      </p>
     </div>
+  </div>
+
   );
 };
 
---
+++ Proyecto/queries.cyp
@@ -0,0 +1,19 @@
+CREATE (doctor1:Doctor {
+  name: "Dr. Pérez",
+  username: "Dr. Pérez",
+  email: "dr.perez@hospital.com",
+  age: 45,
+  specialty: "Cirugía general",
+  password: "contraseña123",
+  website: "www.drperez.com"
+});
+
+CREATE (doctor2:Doctor {
+  name: "Dr. García",
+  username: "Dr. García",
+  email: "dr.garcia@hospital.com",
+  age: 35,
+  specialty: "Pediatría",
+  password: "contraseña456",
+  website: "www.drgarcia.com"
+});
